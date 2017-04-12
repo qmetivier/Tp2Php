9a3627fe325febc04d1a14493567f5c8b8484419
Quentin.Metivier <q.metivier@iia-laval.fr>
2017-04-12 14:57:34 +0200

    Tp2PHP

--- index.php
+++ index.php
@@ -9,30 +9,34 @@ if (isset($_POST['login'])) {
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>Page 1</title>
+
 </head>
 <body>
-	<h1>Identification</h1>
+	<h1 class="title">Identification</h1>
 
-	<?php require 'function.php'; ?>
-
-
-	<form method="post" action="page1.php">
-
+<div class="identification">
+	<div class="element error"><?php require 'function.php'; ?></div>
+	<form method="post" action="index.php">
+			<br>
 		<div class="element">
 			<label for="e1" >Login : </label>
 			<span><input id="e1" type="text" size="25" name="login"
 				value="<?php NameSave('login') ?>"></span>
 			</div>
-			<br>
+				<br>
 			<div class="element">
 				<label for="e2" >Mot de passe : </label>
 				<span><input id="e2" type="password" size="25" name="password"></span>
 			</div>
-			<br>
-			<input id="Send" type="submit" value="Envoyer le formulaire">
-
+				<br>
+			<div class="Send">
+				<input id="Send" type="submit" value="Se Connecter">
+			</div>
+				<br>
 		</form>
-
+</div>
 	</body>
 	</html>
--- page3.php
+++ page3.php
@@ -9,20 +9,20 @@ if (!isset($_SESSION['login'])) {
 <head>
 	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<title>Page 2</title>
+	<title>Page 3</title>
 </head>
 <body>
 
-	<h1 class="title">Page 2</h1>
+	<h1 class="title">Page 3</h1>
 	<br>
 	<div class="identification">
-		<h3>
+		<h3 class="title3">
 			<?php 
 			echo "<span> Bonjour ".$_SESSION['login']."</span>";
 			?>
 		</h3>
 		<div class="swap">
-			<a href="page3.php">Go to Page 3</a>
+			<a href="page2.php">Go to Page 2</a>
 		</div>
 		<form method="post" action="LogOut.php">
 			<div class="Send">
